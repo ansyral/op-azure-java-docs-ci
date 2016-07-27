026dcae399d4cdf3643be5e6899c78834f1c5044
xuzho <xuzho@microsoft.com>
2016-07-27 15:12:52 +0800

    update script since documentation repo's structure changed

--- build-metadata.ps1
+++ build-metadata.ps1
@@ -3,38 +3,28 @@ $ErrorActionPreference = 'Stop'
 
 $scriptPath = $MyInvocation.MyCommand.Path
 $scriptHome = Split-Path $scriptPath
+$azurejavadocs = "OP-Azure-Java-Documentation"
 
 Push-Location $scriptHome
-
-$azurejavasdk = "azure-sdk-for-java"
-
 
 # get and update code2yaml.json
 $user = [Environment]::UserName
 echo $user
-& git clone "git@github.com:ansyral/OP-Azure-Java-Documentation.git"
+& git clone "git@github.com:ansyral/OP-Azure-Java-Documentation.git" $azurejavadocs
 if ($lastexitcode -ne 0)
 {
     exit $lastexitcode
 }
-$config = Get-Content OP-Azure-Java-Documentation\code2yaml.json -Raw | ConvertFrom-Json
-$config.input_path = $azurejavasdk
-$config.exclude_paths = @(Join-Path $azurejavasdk -ChildPath "examples")
-$config.output_path = "_javadocs"
-$config | ConvertTo-Json | Out-File "code2yaml.json"
 
-# run code2yaml to generate metadata yaml files
-& Microsoft.Content.Build.Code2Yaml code2yaml.json
+# call Scripts/build.ps1 to generate metadata yaml files
+& $azurejavadocs\Scripts\build.ps1
 
 if ($lastexitcode -ne 0)
 {
     exit $lastexitcode
 }
 # update to remote repo
-Remove-Item "OP-Azure-Java-Documentation\JavaDoc\api\*" -recurse
-Copy-Item -Path "_javadocs\*" -Destination "OP-Azure-Java-Documentation\JavaDoc\api" -Force
-
-Push-Location "OP-Azure-Java-Documentation"
+Push-Location $azurejavadocs
 & git add .
 & git commit -m "CI Updates"
 & git push -u origin live
